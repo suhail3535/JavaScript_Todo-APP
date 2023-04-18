5a028198dbc85e165c42cacff87b4e6080aa90b7
suhail3535 <khansohail015@gmail.com>
2023-04-18 08:30:23 +0530

    todoupdate

---
+++ Todo-List-App-with-Javascript-main/script.js
@@ -0,0 +1,62 @@
+const form = document.querySelector("form");
+const input = document.querySelector("form input");
+const tasksContainer = document.querySelector(".tasks");
+
+let todos = [];
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const todo = {
+        text: input.value,
+        checked: false,
+        id: new Date().getTime(),
+    };
+
+    todos.push(todo);
+
+    e.target.reset();
+
+    displayTodos();
+});
+
+const displayTodos = () => {
+    tasksContainer.innerHTML = "";
+
+    todos.forEach((todo) => {
+        const taskEl = document.createElement("div");
+        const inputEl = document.createElement("input");
+        const textEl = document.createElement("p");
+        const delButton = document.createElement("button");
+
+        taskEl.classList.add("task");
+        inputEl.classList.add("checkbox");
+        delButton.classList.add("delete");
+        textEl.classList.add("text");
+
+        inputEl.type = "checkbox";
+        textEl.innerHTML = todo.text;
+        delButton.innerHTML = "Delete";
+
+        delButton.addEventListener("click", () => {
+            todos = todos.filter((t) => t.id !== todo.id);
+            displayTodos();
+        });
+
+        inputEl.addEventListener("change", (e) => {
+            todo.checked = e.target.checked;
+            console.log(e.target);
+
+            if (todo.checked) {
+                taskEl.classList.add("done");
+            } else {
+                taskEl.classList.remove("done");
+            }
+        });
+
+        taskEl.appendChild(inputEl);
+        taskEl.appendChild(textEl);
+        taskEl.appendChild(delButton);
+        tasksContainer.appendChild(taskEl);
+    });
+};
